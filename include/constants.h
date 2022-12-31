deb620205d511d823ea728efd212399adaada932
Tasnia Iffat <tasniaiffat@gmail.com>
2022-12-31 22:18:05 +0600

    modifications

--- include/constants.h
+++ include/constants.h
@@ -1,4 +1,9 @@
 #pragma once
 
 #include "headers.h"
-//Here, all the global variables are
+//Here, all the global variables are declared
+
+// Screen dimension constants
+extern const int SCREEN_WIDTH;
+extern const int SCREEN_HEIGHT;
+
